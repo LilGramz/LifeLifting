521a19e2fb05c051541ee7cdda83a017c15faf36
Justyce <s1312480@monmouth.edu>
2023-11-17 17:05:52 -0500

    Initial Commit

---
+++ LifeLifting/LifeLiftingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLiftingApp.swift
+//  LifeLifting
+//
+//  Created by Justyce Graham on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LifeLiftingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
